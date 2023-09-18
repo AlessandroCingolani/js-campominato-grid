30147ecea27baf34446e50ec4d6b53d854edda6b
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-09-18 15:20:17 +0200

    [add]:generated btn for start

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
 const container = document.querySelector('.container')
+const choiseLevel = document.querySelector('.choise-level')
 
 reset();
 
 
-for(let i = 1; i <= 100;i++) {
-  const square = genSquare(i);
-  
-  square.addEventListener('click',function(){
-    console.log(this.id);
-    this.classList.toggle('checked')
-  });
-  
-  container.append(square);
+function init(){
+  for(let i = 1; i <= 100;i++) {
+    const square = genSquare(i);
+    
+    square.addEventListener('click',function(){
+      console.log(this.id);
+      this.classList.toggle('checked')
+    });
+    
+    container.append(square);
+  }
 }
 
 
@@ -19,6 +22,17 @@ for(let i = 1; i <= 100;i++) {
 
 
 
+// generator start btn
+function genBtnStart (){
+  const btn = document.createElement('button');
+  btn.className = 'btn-play'
+  btn.innerHTML = 'Start';
+  btn.addEventListener('click',function(){
+    container.innerHTML = '';
+    init()
+  })
+  return btn;
+}
 
 // square generator 
 function genSquare(index){
@@ -32,4 +46,6 @@ function genSquare(index){
 // reset function
 function reset(){
   container.innerHTML ='';
+  choiseLevel.innerHTML ='';
+  choiseLevel.append(genBtnStart());
 }
