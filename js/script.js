8f052ffe94b38b184ca01a2f23050a8410774201
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-09-18 14:57:03 +0200

    [add]: id for single square

--- js/script.js
+++ js/script.js
@@ -3,13 +3,33 @@ const container = document.querySelector('.container')
 reset();
 
 
-for(let i = 0; i < 100;i++) {
-  const square = document.createElement('div');
-  square.className = 'square';
-  container.append(square)
+for(let i = 1; i <= 100;i++) {
+  const square = genSquare(i);
+  
+  square.addEventListener('click',function(){
+    console.log(this.id);
+    this.classList.toggle('checked')
+  });
+  
+  container.append(square);
 }
 
 
+
+
+
+
+
+// square generator 
+function genSquare(index){
+  const newSquare = document.createElement('div');
+  newSquare.className = 'square';
+  newSquare.id = index;
+  return newSquare;
+}
+
+
+// reset function
 function reset(){
   container.innerHTML ='';
 }
