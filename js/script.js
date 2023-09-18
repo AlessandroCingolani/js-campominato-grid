302cb85d665f15ee48c1b0d6d25a0aa45b9b95a0
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-09-18 14:30:56 +0200

    [add]:square with loop

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ const container = document.querySelector('.container')
 reset();
 
 
-// for(let i = 0; i < 100;i++) {
-//   const square =
-// }
+for(let i = 0; i < 100;i++) {
+  const square = document.createElement('div');
+  square.className = 'square';
+  container.append(square)
+}
 
 
 function reset(){
