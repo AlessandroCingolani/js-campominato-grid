9a7319547d3a7c28f3aaef28b829f7f131ddf085
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-09-18 14:25:19 +0200

    [add]:function reset

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const container = document.querySelector('.container')
+
+reset();
+
+
+// for(let i = 0; i < 100;i++) {
+//   const square =
+// }
+
+
+function reset(){
+  container.innerHTML ='';
+}
